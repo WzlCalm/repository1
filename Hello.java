bfb3e0db345df475dd622accfe0bd7a36359d9b4
WzlCalm <1510439002@qq.com>
2019-10-17 15:07:21 +0800

    第二次提交，添加一个hello.java

---
+++ Hello.java
@@ -0,0 +1,10 @@
+package hello;
+
+import java.sql.SQLOutput;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("第二次提交");
+    }
+}
